af63dc7df10a0eb59a2d79598de58dbc43b55c88
Suada Haji <suada.haji@andela.com>
2016-04-12 17:52:43 +0300

    Integrate paypal in the application

--- app/src/main/java/com/andela/payonly/MainActivity.java
+++ app/src/main/java/com/andela/payonly/MainActivity.java
@@ -1,13 +1,85 @@
 package com.andela.payonly;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CONFIG_CLIENT_ID = "Aa7l9ynGKh_V3MBKXqZjynzjvtH7mYQw0IVONqhdr6Asvy_aiOCzX-ahx15yawkO3E_JNn_R_dcbla_G";
+
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+        .environment(CONFIG_ENVIRONMENT)
+        .clientId(CONFIG_CLIENT_ID);
+
+        PayPalPayment thingToBuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+
     }
-}
+
+    public void makePayment(View view) {
+        thingToBuy = new PayPalPayment(new BigDecimal("10"), "USD",
+                "HeadSet", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(MainActivity.this,
+                PaymentActivity.class);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm = data
+                        .getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println(confirm.toJSONObject().toString(4));
+                        System.out.println(confirm.getPayment().toJSONObject()
+                                .toString(4));
+                        Toast.makeText(getApplicationContext(), "Order placed",
+                                Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                System.out.println("The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                System.out.println("An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        // Stop service when done
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+   }
